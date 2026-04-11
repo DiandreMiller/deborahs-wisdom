501898272de613e06307d3243b0a88837f00b9dc
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2026-04-11 18:01:15 -0400

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
 
-
+// Pages
 import DeborahGamePage from "./Pages/DeborahGamePlay";
-import './App.css'
 import DeborahHomePage from "./Pages/DeborahHomePage";
 import FOUROFOUR from "./Pages/FOUROFOUR";
 
+// Commons
+import Footer from "./commons/Footer";
+
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DeborahHomePage />} path='/' />
-          <Route element={<DeborahGamePage />} path='/play' />
-          <Route element={<FOUROFOUR />} path='*' />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        
+        {/* Main content */}
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<DeborahHomePage />} />
+            <Route path="/play" element={<DeborahGamePage />} />
+            <Route path="*" element={<FOUROFOUR />} />
+          </Routes>
+        </main>
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
