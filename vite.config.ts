a33132bc9bb530e280a17f856c8f9311a86a95b5
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2026-04-11 19:54:16 -0400

    offline access

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['og-image.png', 'robots.txt', 'sitemap.xml'],
+      manifest: {
+        name: "Deborah's Wisdom",
+        short_name: 'Wisdom',
+        description:
+          'A Bible word game where you guess the hidden word and reveal inspiring verses.',
+        theme_color: '#be8f48',
+        background_color: '#fff7dc',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/og-image.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/og-image.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/og-image.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,mp3}'],
+      },
+    }),
+  ],
 })
